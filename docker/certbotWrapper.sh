e2732a7751d1c9cbe1e7acadd24170f29cf397c6
Julian Kratzmann <julian@kratzmann.info>
2024-04-17 22:51:47 +0200

    Fix wrapper

--- docker/certbotWrapper.sh
+++ docker/certbotWrapper.sh
@@ -17,7 +17,7 @@ echo '{
 
 $(dirname "$0")/strato-certbot/auth-hook.py "$@"
 
-rm strato-auth.json
+rm $(dirname "$0")/strato-certbot/strato-auth.json
 
 curl -X POST --insecure -H "Content-Type: application/json" -d \
   '{"message": "Finished '"$TIMESTAMP"' setup strato for new cert", "number": "'"$SIGNAL_SOURCE_NUMBER"'", "recipients": ["'"$SIGNAL_TARGET_NUMBER"'"]}' \
