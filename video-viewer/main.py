f9aeeeef19b123858b7daf1328173c31008df643
Julian Kratzmann <julian@kratzmann.info>
2024-06-22 23:58:35 +0200

    Video viewer tool

---
+++ video-viewer/main.py
@@ -0,0 +1,145 @@
+from flask import Flask, request, send_from_directory, redirect
+import yaml
+import os
+from pythonjsonlogger import jsonlogger
+import logging
+
+
+app = Flask(__name__)
+
+# Configure root logging
+rootLogger = logging.getLogger()
+jsonHandler = logging.StreamHandler()
+formatter = jsonlogger.JsonFormatter()
+jsonHandler.setFormatter(formatter)
+rootLogger.addHandler(jsonHandler)
+rootLogger.setLevel(logging.INFO)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/')
+def home():
+    with open('data/videos.yaml', 'r') as file:
+        videos = yaml.safe_load(file)['videos']
+    html = '<!DOCTYPE html><html><body>'
+    html += generate_reset_button_html()
+    html += generate_video_html(videos)
+    html += generate_form_html()
+    html += generate_delete_toggle_html()
+    return html
+
+@app.route('/', methods=['POST'])
+def add_video():
+    title = request.form.get('title')
+    youtube_id = request.form.get('youtube_id')
+    new_video = {'title': title, 'youtube_id': youtube_id}
+
+    with open('data/videos.yaml', 'r') as file:
+        videos = yaml.safe_load(file)
+
+    # Check if the title or YouTube ID already exists
+    for video in videos['videos']:
+        if video['title'] == title or video['youtube_id'] == youtube_id:
+            return 'Conflict: Video with this title or YouTube ID already exists', 409
+
+    videos['videos'].append(new_video)
+
+    with open('data/videos.yaml', 'w') as file:
+        yaml.dump(videos, file)
+
+    return redirect('/', code=302)
+
+@app.route('/delete_video/<youtube_id>', methods=['POST'])
+def delete_video(youtube_id):
+    with open('data/videos.yaml', 'r') as file:
+        videos = yaml.safe_load(file)
+
+    # Find the video with the matching YouTube ID and remove it
+    videos['videos'] = [video for video in videos['videos'] if video['youtube_id'] != youtube_id]
+
+    with open('data/videos.yaml', 'w') as file:
+        yaml.dump(videos, file)
+
+    return redirect('/', code=302)
+
+def generate_reset_button_html():
+    return '''
+    <button onclick="resetPage()">Reset</button>
+    <script>
+    function resetPage() {
+        // Reset the videos
+        var iframes = document.getElementsByTagName('iframe');
+        for (var i = 0; i < iframes.length; i++) {
+            iframes[i].src = iframes[i].src;
+        }
+
+        // Turn off deletion
+        var deleteButtons = document.getElementsByClassName('delete-button');
+        for (var i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].style.display = 'none';
+        }
+
+        // Uncheck the delete toggle
+        document.getElementById('delete-toggle').checked = false;
+    }
+    </script>
+    '''
+    
+def generate_video_html(videos):
+    html = ''
+    for video in videos:
+        html += f'<h1>{video["title"]}<button class="delete-button" style="display: none;" onclick="deleteVideo(\'{video["youtube_id"]}\')">Delete</button></h1>'
+        html += '''
+        <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
+            <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        '''.format(video["youtube_id"])
+    return html
+
+def generate_form_html():
+    return '''
+    <form method="POST" action="/" oninput="validateForm()">
+        <label for="title">Title:</label><br>
+        <input type="text" id="title" name="title"><br>
+        <label for="youtube_id">YouTube ID:</label><br>
+        <input type="text" id="youtube_id" name="youtube_id"><br>
+        <input type="submit" id="submit" value="Submit" disabled>
+    </form>
+    <script>
+    function validateForm() {
+        var title = document.getElementById('title').value;
+        var youtube_id = document.getElementById('youtube_id').value;
+        document.getElementById('submit').disabled = !(title && youtube_id);
+    }
+    </script>
+    '''
+
+def generate_delete_toggle_html():
+    return '''
+    <input type="checkbox" id="delete-toggle" onchange="toggleDelete()">Enable deletion of videos
+    <script>
+    function toggleDelete() {
+        var deleteButtons = document.getElementsByClassName('delete-button');
+        for (var i = 0; i < deleteButtons.length; i++) {
+            deleteButtons[i].style.display = document.getElementById('delete-toggle').checked ? 'block' : 'none';
+        }
+    }
+    function deleteVideo(youtube_id) {
+        fetch('/delete_video/' + youtube_id, {method: 'POST'})
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                location.reload();
+            })
+            .catch(error => console.error('There has been a problem with your fetch operation: ', error));
+        }
+    </script>
+    '''
+
+# Start the server on port 3000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
