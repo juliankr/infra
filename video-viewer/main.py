e56ba4e67626c7e6b625e2a896c078a0e082233d
Julian Kratzmann <julian@kratzmann.info>
2024-06-24 23:20:30 +0200

    Improve code

--- video-viewer/main.py
+++ video-viewer/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, redirect
+from flask import Flask, request, send_from_directory, redirect, jsonify
 import yaml
 import os
 from pythonjsonlogger import jsonlogger
@@ -20,19 +20,18 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+
 @app.route('/')
 def home():
+    return send_from_directory('static', 'index.html')
+
+
+@app.route('/videos')
+def videos():
     with open('data/videos.yaml', 'r') as file:
-        videos = yaml.safe_load(file)['videos']
-    html = '<!DOCTYPE html><html>'
-    html += generate_head_html()
-    html += '<body>'
-    html += generate_reset_button_html()
-    html += generate_video_html(videos)
-    html += generate_form_html()
-    html += generate_delete_toggle_html()
-    html += '</body></html>'
-    return html
+        videos = yaml.safe_load(file)
+    return jsonify(videos)
+
 
 @app.route('/', methods=['POST'])
 def add_video():
@@ -55,6 +54,7 @@ def add_video():
 
     return redirect('/', code=302)
 
+
 @app.route('/delete_video/<youtube_id>', methods=['POST'])
 def delete_video(youtube_id):
     with open('data/videos.yaml', 'r') as file:
@@ -67,131 +67,6 @@ def delete_video(youtube_id):
         yaml.dump(videos, file)
 
     return redirect('/', code=302)
-
-def generate_reset_button_html():
-    return '''
-    <style>
-    button {
-        padding: 15px 25px;
-        font-size: 24px;
-        text-align: center;
-        cursor: pointer;
-        outline: none;
-        color: #fff;
-        background-color: #4CAF50;
-        border: none;
-        border-radius: 15px;
-        box-shadow: 0 9px #999;
-    }
-
-    button:hover {background-color: #3e8e41}
-
-    button:active {
-        background-color: #3e8e41;
-        box-shadow: 0 5px #666;
-        transform: translateY(4px);
-    }
-    </style>
-    <button onclick="resetPage()">Reset</button>
-    <script>
-    function resetPage() {
-        // Reset the videos
-        var iframes = document.getElementsByTagName('iframe');
-        for (var i = 0; i < iframes.length; i++) {
-            iframes[i].src = iframes[i].src;
-        }
-
-        // Turn off deletion
-        var deleteButtons = document.getElementsByClassName('delete-button');
-        for (var i = 0; i < deleteButtons.length; i++) {
-            deleteButtons[i].style.display = 'none';
-        }
-
-        // Uncheck the delete toggle
-        document.getElementById('delete-toggle').checked = false;
-    }
-    </script>
-    '''
-    
-def generate_video_html(videos):
-    html = ''
-    for video in videos:
-        html += f'<h1>{video["title"]}<button class="delete-button" style="display: none;" onclick="deleteVideo(\'{video["youtube_id"]}\')">Delete</button></h1>'
-        html += '''
-        <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
-            <iframe style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://www.youtube.com/embed/{}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
-        '''.format(video["youtube_id"])
-    return html
-
-def generate_form_html():
-    return '''
-     <style>
-    input[type="text"], input[type="submit"] {
-        width: 100%;
-        padding: 12px 20px;
-        margin: 8px 0;
-        display: inline-block;
-        border: 1px solid #ccc;
-        border-radius: 4px;
-        box-sizing: border-box;
-        font-size: 18px;
-    }
-    </style>
-    <form method="POST" action="/" oninput="validateForm()">
-        <label for="title">Title:</label><br>
-        <input type="text" id="title" name="title"><br>
-        <label for="youtube_id">YouTube ID:</label><br>
-        <input type="text" id="youtube_id" name="youtube_id"><br>
-        <input type="submit" id="submit" value="Submit" disabled>
-    </form>
-    <script>
-    function validateForm() {
-        var title = document.getElementById('title').value;
-        var youtube_id = document.getElementById('youtube_id').value;
-        document.getElementById('submit').disabled = !(title && youtube_id);
-    }
-    </script>
-    '''
-
-def generate_delete_toggle_html():
-    return '''
-    <style>
-    input[type="checkbox"] {
-        width: 25px;
-        height: 25px;
-    }
-    </style>
-    <input type="checkbox" id="delete-toggle" onchange="toggleDelete()">Enable deletion of videos
-    <script>
-    function toggleDelete() {
-        var deleteButtons = document.getElementsByClassName('delete-button');
-        for (var i = 0; i < deleteButtons.length; i++) {
-            deleteButtons[i].style.display = document.getElementById('delete-toggle').checked ? 'block' : 'none';
-        }
-    }
-    function deleteVideo(youtube_id) {
-        fetch('/delete_video/' + youtube_id, {method: 'POST'})
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                location.reload();
-            })
-            .catch(error => console.error('There has been a problem with your fetch operation: ', error));
-        }
-    </script>
-    '''
-
-def generate_head_html():
-    return '''
-    <head>
-        <meta name="apple-mobile-web-app-title" content="Video-viewer">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black">
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <link rel="apple-touch-icon" href="static/icon.png">
-    </head>
-    '''
 
 # Start the server on port 3000
 if __name__ == '__main__':
---
+++ video-viewer/static/main.js
@@ -0,0 +1,58 @@
+function resetPage() {
+    // Reset the videos
+    var iframes = document.getElementsByTagName('iframe');
+    for (var i = 0; i < iframes.length; i++) {
+        iframes[i].src = iframes[i].src;
+    }
+
+    // Turn off deletion
+    var deleteButtons = document.getElementsByClassName('delete-button');
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].style.display = 'none';
+    }
+
+    // Uncheck the delete toggle
+    document.getElementById('delete-toggle').checked = false;
+}
+
+function validateForm() {
+    var title = document.getElementById('title').value;
+    var youtube_id = document.getElementById('youtube_id').value;
+    document.getElementById('submit').disabled = !(title && youtube_id);
+}
+
+function toggleDelete() {
+    var deleteButtons = document.getElementsByClassName('delete-button');
+    for (var i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].style.display = document.getElementById('delete-toggle').checked ? 'block' : 'none';
+    }
+}
+function deleteVideo(youtube_id) {
+    fetch('/delete_video/' + youtube_id, {method: 'POST'})
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            location.reload();
+        })
+    .catch(error => console.error('There has been a problem with your fetch operation: ', error));
+}
+
+window.onload = function() {
+    fetch('/videos')
+        .then(response => response.json())
+        .then(data => {
+            var videoList = document.getElementById('video-list');
+            data.videos.forEach(video => {
+                var videoElement = document.createElement('div');
+                videoElement.innerHTML = `
+                    <h1>${video.title}<button class="delete-button" onclick="deleteVideo('${video.youtube_id}')">Delete</button></h1>
+                    <div class="video-container">
+                        <iframe class="video-frame" src="https://www.youtube.com/embed/${video.youtube_id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                `;
+                videoList.appendChild(videoElement);
+            });
+        });
+}
+
